af465b8d7bdb24fd1dc63ba7c74e629ac87ecf2a
esther8283 <esther8283@naver.com>
2021-01-14 14:22:22 +0900

    String 클래스

---
+++ src/ch11/StringClassmain.java
@@ -0,0 +1,21 @@
+package ch11;
+
+public class StringClassmain {
+
+	public static void main(String[] args) {
+		StringClass sc = new StringClass();
+		
+		sc.stringCharAt();
+		sc.stringEquals();
+		sc.stringGetBytes();
+		sc.stringIndexOf();
+		sc.stringLength();
+		sc.stringReplace();
+		sc.stringSubstring();
+		sc.stringToLower();
+		sc.stringTrim();
+		sc.stringValueOf();
+		
+	}
+
+}
